e02d7d41e10fa2983294d3703a52176f62359e35
mr-volch <mr.volchaninov@yandex.ru>
2024-05-20 11:08:13 +0300

    Окончательно добавлена поддержка использования алгоритмов автоматизации определения количества измерений задержки.

--- src/core/string_id_converters.h
+++ src/core/string_id_converters.h
@@ -4,38 +4,31 @@
 /*
  * Data types
  */
-#define NUM_NETWORK_TEST_DATATYPES 4
+#define NUM_NETWORK_TEST_DATATYPES 6
 
-#define AVERAGE_NETWORK_TEST_DATATYPE   1
-#define MEDIAN_NETWORK_TEST_DATATYPE    2
-#define DEVIATION_NETWORK_TEST_DATATYPE 3
-#define MIN_NETWORK_TEST_DATATYPE       4
+#define AVERAGE_NETWORK_TEST_DATATYPE     1
+#define MEDIAN_NETWORK_TEST_DATATYPE      2
+#define DEVIATION_NETWORK_TEST_DATATYPE   3
+#define MIN_NETWORK_TEST_DATATYPE         4
+#define ALL_DELAYS_NETWORK_TEST_DATATYPE 5
+#define MEASUREMENTS_AMOUNT_TEST_DATATYPE 6
 
 /*
  * Test types
  */
 
-#define NUM_TEST_TYPES 9
 
-#define UNKNOWN_TEST_TYPE 0
-#define ONE_TO_ONE_TEST_TYPE 1
-#define ALL_TO_ALL_TEST_TYPE 2
-#define ASYNC_ONE_TO_ONE_TEST_TYPE 3
-#define SEND_RECV_AND_RECV_SEND_TEST_TYPE 4
-#define NOISE_TEST_TYPE 5
-#define NOISE_BLOCKING_TEST_TYPE 6
-#define BCAST_TEST_TYPE 7
-#define PUT_ONE_TO_ONE_TEST_TYPE 8
-#define GET_ONE_TO_ONE_TEST_TYPE 9
-
-
+//это для того чтобы при компиляции на плюсах 
+//стандарт вызова был под си?
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-
-extern const char *file_data_type_to_sring(const int data_type);
+//функция для вывода типа содержимого файла
+//(среднее, медиана, ... )
+extern const char *file_data_type_to_string(const int data_type);
+//где эти описаны/используются?
 extern int get_test_type(const char *str);
 extern int get_test_type_name(int test_type,char *str);
 
